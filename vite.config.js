2acfe41b2fbd31a4c7c4422d92e9559c12712800
FhriiPhri <fahripadang050908@gmail.com>
2026-01-21 11:35:45 +0700

    fix bug: rewerite /api for prod & local

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,12 @@ export default ({ mode }) => {
   return defineConfig({
     plugins: [react()],
     server: {
+      // vite.config.js
       proxy: {
         "/api": {
-          target: env.VITE_API_URL,
+          target: env.VITE_API_BASE_URL,
           changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""), // Menghapus /api dari request frontend
         },
       },
     },
